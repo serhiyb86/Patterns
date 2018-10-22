e3fa395eb2f42801cf05db485021f0458496201b
rkorchak <rkorch@softserveinc.com>
2018-10-22 13:22:26 +0300

    IF-1876 updated ResponseNotification model for BookOff request

--- src/main/java/com/motorola/models/representation/ResponseNotification.java
+++ src/main/java/com/motorola/models/representation/ResponseNotification.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Response notification SB message
@@ -13,7 +15,118 @@ import java.io.Serializable;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(NON_NULL)
 public class ResponseNotification implements Serializable {
-    /** Default serial version ID. */
-    private static final long serialVersionUID = 1L;
+
+	/** Default serial version ID. */
+	private static final long serialVersionUID = 1L;
+
+	private String customerId;
+
+	private String notificationType;
+
+	private String whenSubmitted;
+
+	private String correlationId;
+
+	private String sessionId;
+
+	private String resultNature;
+
+	private String responseType;
+
+	private String key;
+
+	private String keyType;
+
+	private ApiError error;
+
+	private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+	public String getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getNotificationType() {
+		return notificationType;
+	}
+
+	public void setNotificationType(String notificationType) {
+		this.notificationType = notificationType;
+	}
+
+	public String getWhenSubmitted() {
+		return whenSubmitted;
+	}
+
+	public void setWhenSubmitted(String whenSubmitted) {
+		this.whenSubmitted = whenSubmitted;
+	}
+
+	public String getCorrelationId() {
+		return correlationId;
+	}
+
+	public void setCorrelationId(String correlationId) {
+		this.correlationId = correlationId;
+	}
+
+	public String getSessionId() {
+		return sessionId;
+	}
+
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+
+	public String getResultNature() {
+		return resultNature;
+	}
+
+	public void setResultNature(String resultNature) {
+		this.resultNature = resultNature;
+	}
+
+	public String getResponseType() {
+		return responseType;
+	}
+
+	public void setResponseType(String responseType) {
+		this.responseType = responseType;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getKeyType() {
+		return keyType;
+	}
+
+	public void setKeyType(String keyType) {
+		this.keyType = keyType;
+	}
+
+	public ApiError getError() {
+		return error;
+	}
+
+	public void setError(ApiError error) {
+		this.error = error;
+	}
+
+	public Map<String, Object> getAdditionalProperties() {
+		return this.additionalProperties;
+	}
+
+	public void setAdditionalProperty(String name, Object value) {
+		this.additionalProperties.put(name, value);
+	}
 
 }
