e75e9eb83b11bdc6eb355dfc7bf15c0c50c7086f
Stas Krasovskyi <skras@softserveinc.com>
2018-12-12 14:20:02 +0200

    IF-2077 Fixed unit tests

--- src/test/java/com/motorola/translation/v2019_1/TranslatorTest.java
+++ src/test/java/com/motorola/translation/v2019_1/TranslatorTest.java
@@ -3,39 +3,37 @@
  */
 package com.motorola.translation.v2019_1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.net.URL;
+
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.motorola.constants.InterfaceConstants;
 import com.motorola.translation.BaseTranslator;
 import com.motorola.translation.TranslatorsFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.net.URL;
 
 /**
  * Unit test for verify translator.
  */
 public class TranslatorTest {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(TranslatorTest.class);
+//	private static final Logger LOGGER = LoggerFactory.getLogger(TranslatorTest.class);
 
-	private BaseTranslator translator = TranslatorsFactory.getTranslator(InterfaceConstants.GeneralProperties.VERSION_2019_1);
+	private BaseTranslator translator = new TranslatorsFactory().getTranslator(InterfaceConstants.GeneralProperties.VERSION_2019_1_15_0);
 
 	public JsonObject initInputPayload(String inputName) {
 		JsonObject result = null;
 		//  read the file input for current translator version (2019.1)
-		String path = InterfaceConstants.GeneralProperties.VERSION_2019_1 + "/" + inputName;
+		String path = InterfaceConstants.GeneralProperties.VERSION_2019_1_15_0 + "/" + inputName;
 		File file = getResourceFile(path);
 		JsonParser parser = new JsonParser();
 		try {
 			result = (JsonObject) parser.parse(new FileReader(file));
 		}
 		catch (FileNotFoundException ex) {
-			LOGGER.error("Parsing input json file error.");
+//			LOGGER.error("Parsing input json file error.");
 		}
 
 		return result;
