572c30fbbdbe94597972bc48007bdb99fb0cdcef
Stas Krasovskyi <skras@softserveinc.com>
2018-12-14 17:00:51 +0200

    IF-2077 Refactored version check code

--- src/main/java/com/motorola/translation/TranslatorVersion.java
+++ src/main/java/com/motorola/translation/TranslatorVersion.java
@@ -3,60 +3,58 @@
  */
 package com.motorola.translation;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
- * Encapsulates methods for parsing and comparing SpillmanApi versions received in request headers.
+ * Parse SpillmanApi version from request header.
+ *
+ * Allows to use shorter versions like: '2019.1.15', '2019.1' or '2019'.
+ * The missing parts will be considered to equal '0' (Exmpl.: '2019.1' => '2019.1.0.0', etc.).
+ * All chunks of unrecognizable values will be skipped ('2019.1.33-SNAPSHOT.5' => '2019.1.5.0', '2019-SNAP-SNAP' => "0.0.0.0").
  */
 class TranslatorVersion {
 
-	private int[] version;
+	private static final String DELIMITER = ".";
+	private static final String DELIMITER_REGEXP = "\\.";
+	private static final int LENGTH = 4;
+	private static final Integer DEFAULT_CHUNK = 0;
 
-	public TranslatorVersion(int major, int minor, int sprint, int build) {
-		this.version = new int[4];
-		this.version[0] = major;
-		this.version[1] = minor;
-		this.version[2] = sprint;
-		this.version[3] = build;
-	}
+	private List<Integer> versionChunks;
 
-	/**
-	 * Parse version string and return new {@link TranslatorVersion}.
-	 * This method allows to use shorter versions like: '2019.1.15', '2019.1' or '2019'.
-	 * The missing parts will be considered to equal '0' (Exmpl.: '2019.1' => '2019.1.0.0', etc.).
-	 * If input string contains unrecognizable value - the process will stop and all remaining parts will be set to '0'.
-	 */
-	public static TranslatorVersion parse(String version) {
-		int[] ver = {0, 0, 0, 0};
-		if (StringUtils.isNotBlank(version)) {
-			try {
-				for (int i = 0; i < 4; i++) {
-					int dotPos = version.indexOf('.');
-					if (dotPos == -1) {
-						dotPos = version.length();
-					}
-					ver[i] = Integer.parseInt(version.substring(0, dotPos));
-					version = version.substring(dotPos + 1);
-				}
-			}
-			catch (NumberFormatException | IndexOutOfBoundsException ex) {
-				// abort parsing process
-			}
+	TranslatorVersion(String version) {
+		List<Integer> ver = Stream.of(version.split(DELIMITER_REGEXP))
+			.filter(chunk -> chunk.matches("\\d+")) //Exclude non-integer values
+			.map(Integer::parseInt)
+			.limit(LENGTH)
+			.collect(Collectors.toList());
+
+		while (ver.size() < LENGTH) {
+			ver.add(DEFAULT_CHUNK);
 		}
-		return new TranslatorVersion(ver[0], ver[1], ver[2], ver[3]);
+		this.versionChunks = ver;
 	}
 
 	/**
 	 * Check if this version is not older than other.
 	 */
-	public boolean notOlderThan(TranslatorVersion version) {
-		boolean result = true;
-		for (int i = 0; i < 4; i++) {
-			if (this.version[i] < version.version[i]) {
-				result = false;
-			}
-			if (!result || this.version[i] > version.version[i]) {
-				break;
+	boolean notLessThan(TranslatorVersion version) {
+		return compare(version) >= 0;
+	}
+
+	/**
+	 * Compare version with another.
+	 *
+	 * @param version {@link TranslatorVersion} obj
+	 * @return 0 - equals, < 0 - when less than, > 0 - greater than.
+	 */
+	private int compare(TranslatorVersion version) {
+		int result = 0;
+		for (int index = 0; index < LENGTH; index++) {
+			result = versionChunks.get(index) - version.versionChunks.get(index);
+			if (result != 0) {
+				return result;
 			}
 		}
 		return result;
@@ -64,6 +62,6 @@ class TranslatorVersion {
 
 	@Override
 	public String toString() {
-		return version[0] + "." + version[1] + "." + version[2] + "." + version[3];
+		return String.join(DELIMITER, versionChunks.stream().map(Object::toString).collect(Collectors.toList()));
 	}
 }
