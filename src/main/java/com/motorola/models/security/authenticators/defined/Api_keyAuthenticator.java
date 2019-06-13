2737dddd02b8c449aa9c53863bce90881a2abdc6
Oleksandr Stino <oleksandr.stino@motorolasolutions.com>
2019-06-13 14:15:54 +0300

    IF-2643 fixed sending requests to the Ingest API

--- src/main/java/com/motorola/models/security/authenticators/defined/Api_keyAuthenticator.java
+++ src/main/java/com/motorola/models/security/authenticators/defined/Api_keyAuthenticator.java
@@ -5,7 +5,7 @@ import com.motorola.models.security.authenticators.HeaderApiKeyAuthenticator;
 public class Api_keyAuthenticator extends HeaderApiKeyAuthenticator {
 
     public Api_keyAuthenticator(String token) {
-        super("api_key", token);
+        super("Authorization", "Bearer " + token);
     }
 
 }
