16e24bd9bbbd1ed68259b0f4ef4a058b1936abd9
Oleksandra Podhurska <ozhluk@softserveinc.com>
2020-05-08 16:19:33 +0300

    IF-3411 Changed whenUpdated setter logic.

--- src/main/java/com/motorola/constants/InterfaceConstants.java
+++ src/main/java/com/motorola/constants/InterfaceConstants.java
@@ -313,6 +313,7 @@ public final class InterfaceConstants {
 			public static final String WHEN_CREATED = "whenCreated";
 			public static final String WHEN_STATUS_EXPIRES = "whenStatusExpires";
 			public static final String IS_ON_DUTY = "isOnDuty";
+			public static final String WHEN_UPDATED = "whenUpdated";
 		}
 
 		public final class AssignedIncident {
